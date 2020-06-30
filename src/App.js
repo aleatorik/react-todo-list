a650d2af3b74e7ef8df27508d576609ea3a0940b
aleatorik <vates0328@gmail.com>
2020-06-30 21:02:20 +0900

    update: Handle submit function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+    setTodos({ todo, ...todos });
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>React Todo</p>
+        <TodoForm addTodo={addTodo} />
       </header>
     </div>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
   const [todo, setTodo] = useState({
     id: '',
     task: '',
@@ -11,8 +11,17 @@ function TodoForm() {
     setTodo({ ...todo, task: e.target.value });
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuid.v4() });
+      // reset task input
+      setTodo({ ...todo, task: '' });
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         name="task"
         type="text"
