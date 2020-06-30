851c9e6c7d5c80087f57de42eb52cbe15edea067
aleatorik <vates0328@gmail.com>
2020-06-30 23:00:17 +0900

    feat: Store data in local storage & render toggle complete and delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const LOCAL_STORAGE_KEY = 'react-todo-list-todos';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  function addTodo() {
-    setTodos({ todo, ...todos });
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
 
   return (
@@ -14,6 +45,11 @@ function App() {
       <header className="App-header">
         <p>React Todo</p>
         <TodoForm addTodo={addTodo} />
+        <TodoList
+          todos={todos}
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}
+        />
       </header>
     </div>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function TodoForm({ addTodo }) {
   const [todo, setTodo] = useState({
@@ -14,7 +15,7 @@ function TodoForm({ addTodo }) {
   function handleSubmit(e) {
     e.preventDefault();
     if (todo.task.trim()) {
-      addTodo({ ...todo, id: uuid.v4() });
+      addTodo({ ...todo, id: uuidv4() });
       // reset task input
       setTodo({ ...todo, task: '' });
     }
@@ -28,7 +29,7 @@ function TodoForm({ addTodo }) {
         value={todo.task}
         onChange={handleTaskInputChange}
       />
-      <button />
+      <button type="submit">submit</button>
     </form>
   );
 }
